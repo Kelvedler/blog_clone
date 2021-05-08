f5f31f98df9d52b111ff182a9e5467a0c1753467
Yaroslav <hnflkmvjcm@gmail.com>
2021-05-08 23:52:06 +0300

    added templates, admin

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from blog import views
 
-app_name = 'blog'
-
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
